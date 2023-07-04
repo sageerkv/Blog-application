eb2b895ee7eca31556fb205c123107320e08b881
sageerkv <sageerkv93@gmail.com>
2023-07-04 11:20:55 +0530

    inital commit

--- Blog/Blog_app/urls.py
+++ Blog/Blog_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView
+from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView, CategoryListView, LikeView
 
 
 urlpatterns = [
@@ -10,5 +10,7 @@ urlpatterns = [
     path('add_category/', AddCategoryView.as_view(), name="add_category"),
     path('article/edit/<int:pk>', UpdatePostView.as_view(), name="update_post"),
     path('article/<int:pk>remove', DeletePostView.as_view(), name="delete_post"),
-    path('category/<str:cats>/', CategoryView, name="category")
+    path('category/<str:cats>/', CategoryView, name="category"),
+    path('category_list/', CategoryListView, name="category_list"),
+    path('like/<int:pk>', LikeView, name='like_post'),
 ]
--- Blog/Blog_app/views.py
+++ Blog/Blog_app/views.py
@@ -1,18 +1,40 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from typing import Any
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseRedirect
 from django.contrib import messages
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .forms import PostForm, EditForm
 from . models import Post, Category
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 
 # Create your views here.
+
+def LikeView(request, pk):
+    post = get_object_or_404(Post, id=request.POST.get('post_id'))
+    liked = False
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+        liked = False
+    else:
+        post.likes.add(request.user)
+        liked = True
+    return  HttpResponseRedirect(reverse('article-detail', args=[str(pk)]))
 
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
     ordering = ['id']
     # ordering = ['post_date']
+
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        context["cat_menu"] = cat_menu
+        return context
+
+def CategoryListView(request):
+    cat_menu_list = Category.objects.all()
+    return render(request, 'category_list.html', {'cat_menu_list': cat_menu_list})
 
 def CategoryView(request, cats):
     category_posts = Post.objects.filter(category=cats)
@@ -22,6 +44,22 @@ def CategoryView(request, cats):
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_details.html'
+
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(ArticleDetailView, self).get_context_data(*args, **kwargs)
+
+        stuff = get_object_or_404(Post, id=self.kwargs['pk'])
+        total_likes = stuff.total_likes()
+
+        liked = False
+        if stuff.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        context["cat_menu"] = cat_menu
+        context["total_likes"] = total_likes
+        context["liked"] = liked
+        return context
 
 class AddPostView(CreateView):
     model = Post
@@ -37,8 +75,9 @@ class AddCategoryView(CreateView):
 
 class UpdatePostView(UpdateView):
     model = Post
+    form_class = EditForm
     template_name = 'update_post.html'
-    fields = ['title', 'title_tag', 'body']
+    # fields = '__all__'
 
 class DeletePostView(DeleteView):
     model = Post
